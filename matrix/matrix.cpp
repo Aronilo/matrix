d1efa69d257bccafb1944b33a7c4ee60a49f43c9
Валерия Каширина ПК <Aronilo@mail.ru>
2021-10-18 23:57:20 +0300

    хоп

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -16,17 +16,37 @@ int main()
 	srand(time(0));
 	for (int i = 0; i < size; ++i)
 		for (int j = 0; j < size; ++j)
-			mass[i][j] = -10 + rand() % 21;		
+			mass[i][j] = rand() % 21 - 10;		
 	for (int i = 0; i < size; ++i)
 	{
 		for (int j = 0; j < size; ++j)
-			cout << mass[i][j] << " ";
+			cout << setw(3) << mass[i][j] << " ";
 		cout << endl;
 	}
+	
+	int total = 0;
+
 	for (int i = 0; i < size; ++i)
 	{
-		mass[i = size - 1-i][j]=mass[;
-		mass[]
+		total += mass[size-1][i]*mass[i][0];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		mass[size - 1 - i][i] = total;
+	}
+
+	cout << endl;
+
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < size; ++j)
+			cout << setw(3) << mass[i][j] << " ";
+		cout << endl;
+	}
+	for (int i = 0; i < size; i++)
+		{
+		delete[] mass[i];
+		}
 
 		delete[] mass;
 }
